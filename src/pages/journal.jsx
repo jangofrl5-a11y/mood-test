2b28a064d0c4875bd583b8e545bfa4790c22f5ac
devnull <devnull@example.com>
2025-09-26 13:00:37 -0400

    feat: add accessible EmailInput to journal page

--- src/pages/journal.jsx
+++ src/pages/journal.jsx
@@ -2,13 +2,15 @@
 import { Box, VStack, Textarea } from '@chakra-ui/react';
 import CalendarView from '../components/calendarview';
 import SaveButton from '../components/SaveButton';
+import EmailInput from '../components/EmailInput';
 
 export default function Journal() {
   return (
     <Box p={8}>
       <VStack spacing={6}>
         <CalendarView />
-        <Textarea placeholder="Write your thoughts here..." />
+  <EmailInput />
+  <Textarea placeholder="Write your thoughts here..." />
         <SaveButton />
       </VStack>
     </Box>
